6fdc3cd41eca8de78fd7b8f08da78dc4d244cabd
Weston Clark <westoclark@gmail.com>
2023-05-22 17:27:16 -0700

    display full song card

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,14 +1,57 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 
-import SongAudio from './SongAudio.jsx';
-import SongNotes from './SongNotes.jsx';
-
-export default function Song({ songName }) {
+export default function Song({ songData }) {
   return (
     <div className="song">
-      <SongAudio songName={songName} />
+      <p id="song-name">{songData.name}</p>
+      <audio id="audio" src={songData.url} controls />
       <SongNotes />
+    </div>
+  );
+}
+
+function SongNotes() {
+  const [notes, setNotes] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  const addNote = (userInput) => {
+    let copy = [...notes];
+    copy = [...copy, userInput];
+    setNotes(copy);
+  };
+
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addNote(userInput);
+    setUserInput('');
+  };
+
+  const notesList = [];
+  for (let i = 0; i < notes.length; i++) {
+    notesList.push(
+      <div id="note" key={i}>
+        <input type="checkbox"></input>
+        <div className="note-text" key={notes[i]}>
+          {notes[i]}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div id="add-note-and-list">
+      <form id="input-and-button">
+        <input value={userInput} type="text" onChange={handleChange}></input>
+        <button id="add-note-button" onClick={handleSubmit}>
+          Add Note
+        </button>
+      </form>
+      <div id="notes-list">{notesList}</div>
     </div>
   );
 }
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -8,18 +8,14 @@ import Song from './Song.jsx';
 
 function SongList() {
   const [queryParameters] = useSearchParams();
-  const [projectId, setProjectId] = useState('');
-  const [projectName, setProjectName] = useState('');
-  const [songList, setSongList] = useState(['abc']);
+  const [projectId, setProjectId] = useState(queryParameters.get('projectId'));
+  const [projectName, setProjectName] = useState(queryParameters.get('projectName'));
+  const [songList, setSongList] = useState([]);
 
   function getSongs() {
     axios.get(`/api/songs/${projectId}`).then((res) => {
       console.log(res);
       setSongList(res.data);
-      // for (let i = 0; i < songList.length; i++) {
-      //   songs.push(<p>{songList[i]}</p>);
-      //   console.log(songList[i]);
-      // }
     });
   }
 
@@ -43,13 +39,10 @@ function SongList() {
     }
 
     handleSubmit();
-    // getSongs();
   }
 
   useEffect(() => {
-    setProjectId(queryParameters.get('projectId'));
-    setProjectName(queryParameters.get('projectName'));
-    // getSongs();
+    getSongs();
   }, []);
 
   let songs = [];
@@ -64,10 +57,11 @@ function SongList() {
             <input id="file" type="file" name="file" onChange={handleFileSelect}></input>
           </label>
         </div>
-        <button id="refresh">Refresh</button>
+        <button id="refresh" onClick={getSongs}>
+          Refresh
+        </button>
       </div>
-      {/* <p>{songList}</p> */}
-      <div id="feed">{songList.length ? songList.map((song) => <p key={song.name}>{song.name}</p>) : null}</div>
+      {songList.length ? songList.map((song) => <Song key={song.id} songData={song}></Song>) : null}
     </div>
   );
 }
